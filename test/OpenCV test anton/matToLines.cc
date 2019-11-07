32d989887f407d9b2482a9a1a854563054302104
Elias <eliasjosse@gmail.com>
2019-11-07 10:23:24 +0100

    matTolines

---
+++ test/OpenCV test anton/matToLines.cc
@@ -0,0 +1,14 @@
+#include "opencv2/opencv.hpp"
+
+
+/* Find lines from Matrix
+src - input matrix
+linesP - output vector of Vec4i lines
+*/
+void matToLines(cv::Mat &src, std::vector<cv::Vec4i>& linesP);
+void matToLines(cv::Mat &src, std::vector<cv::Vec4i> &linesP) {
+	cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+	cv::GaussianBlur(src, src, { 5, 5 }, 0);
+	cv::Canny(src,src, 50, 150);
+	cv::HoughLinesP(src, linesP, 1, CV_PI / 360, 20, 10, 100);
+}
