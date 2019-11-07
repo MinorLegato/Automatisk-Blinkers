974acac7e2b5392a8e052979f7101a77334ed10e
Anton Siderud <anton.siderud@gmail.com>
2019-11-07 10:37:19 +0100

    asdf

--- test/OpenCV test anton/main.cc
+++ test/OpenCV test anton/main.cc
@@ -1,13 +1,14 @@
 #include "common.cc"
+#include "matToLines.cc"
 
 int main(void) {
     cv::VideoCapture cap(0);
 
     if (!cap.isOpened()) { return -1; }
 
-    cv::Mat capture, edges;
+    cv::Mat capture;
 
-    std::vector<cv::Vec4i>  line_vector;
+    std::vector<cv::Vec4i> line_vector;
     line_vector.reserve(8 * 2048);
 
     LineGrid grid;
@@ -16,30 +17,24 @@ int main(void) {
     while (cv::waitKey(16) != 27) {
         cap >> capture;
 
-        cv::Mat lines = cv::Mat::zeros(capture.size(), capture.type());
+        matToLines(capture, line_vector);
 
-        cv::cvtColor(capture, capture, cv::COLOR_BGR2GRAY);
-        cv::GaussianBlur(capture, edges, { 5, 5 }, 0);
-        cv::Canny(edges, edges, 50, 150);
+        cv::Mat lines = cv::Mat::zeros(capture.size(), capture.type());
 
         // find lines:
         {
-            line_vector.resize(0);
 
             resizeGrid(&grid, capture.cols, capture.rows);
             clearGrid(&grid);
 
-            cv::HoughLinesP(edges, line_vector, 2, CV_PI / 180, 20, 32, 16);
-
             for (auto line : line_vector) {
-                cv::line(lines, { line[0], line[1] }, { line[2], line[3] }, { 0, 100, 255 }, 2);
+                cv::line(lines, { line[0], line[1] }, { line[2], line[3] }, { 255 }, 2);
                 addLineToGrid(&grid, line);
             }
         }
 
         {
             system("cls");
-
             printf("%d\n", (int)line_vector.size());
 
             for (int y = 0; y < grid.height; ++y) {
@@ -60,7 +55,7 @@ int main(void) {
         }
 
         cv::imshow("capture", capture);
-        cv::imshow("edges", edges);
+        //cv::imshow("edges", edges);
         cv::imshow("lines", lines);
     }
 }
--- test/OpenCV test anton/matToLines.cc
+++ test/OpenCV test anton/matToLines.cc
@@ -1,14 +1,15 @@
-#include "opencv2/opencv.hpp"
-
+#pragma once
 
 /* Find lines from Matrix
 src - input matrix
 linesP - output vector of Vec4i lines
 */
-void matToLines(cv::Mat &src, std::vector<cv::Vec4i>& linesP);
+
 void matToLines(cv::Mat &src, std::vector<cv::Vec4i> &linesP) {
+    linesP.resize(0);
+
 	cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
 	cv::GaussianBlur(src, src, { 5, 5 }, 0);
-	cv::Canny(src,src, 50, 150);
+	cv::Canny(src, src, 50, 150);
 	cv::HoughLinesP(src, linesP, 1, CV_PI / 360, 20, 10, 100);
 }
