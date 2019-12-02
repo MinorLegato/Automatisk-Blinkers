100fb742361dc1a562886955ad796e85bada6bff
Anton Siderud <anton.siderud@gmail.com>
2019-12-02 17:53:55 +0100

    crc almost done

--- test/crc test/main.c
+++ test/crc test/main.c
@@ -1,9 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
 
-static unsigned Crc32(const void *data, size_t size)
+static uint32_t CRC32Code(const void *data, size_t size)
 {
-    unsigned result = 0;
-    //
-    return result;
+    const uint8_t *d  = data;
+    uint32_t      crc = 0xFFFFFFFF;
+
+    while (size--)
+    {
+        uint32_t index = (crc ^ *(d++)) & 0xFF;
+
+        crc = (crc >> 8) ^ crc_table[index];
+    }
+
+    return crc;
 }
 
 int main(void)
