203b10e17f5abed598f913d4fa11cb2f604ea876
MinorLegato <anton.siderud@gmail.com>
2019-12-05 16:07:57 +0100

    ändrad:        main/main.cc

--- main/main.cc
+++ main/main.cc
@@ -1,8 +1,11 @@
-
+#include "lib/common.cc"
+#include "lib/klass.cc"
+#include "lib/matToLines.cc"
 #include "canlib.h"
 #include "net.h"
 
 #include <stdint.h>
+#include <thread>
 
 #define ARRAY_COUNT(array) (sizeof (array) / sizeof (array[0]))
 
@@ -13,11 +16,151 @@ struct Controller
 	int8_t 	blink;
 };
 
-//unsigned char data[] = { 0, 1, 2, 3, 4, 5, 6 };
-//canSend(&can, 0x7DF, data, ARRAY_COUNT(data));
+static Controller controller = {0};
+
+static void ControllerThread(void)
+{
+	Can 		can         = {0};
+	Server 		server      = {0};
+
+    puts("can");
+	canInit(&can);
+    puts("net");
+	netServerInit(&server, 8888);
+
+	while (1) {
+        puts("net recv");
+		netServerRecv(&server, &controller, sizeof (Controller));
+
+		printf("t %d\n", controller.thrust);
+		printf("s %d\n", controller.steering);
+		printf("b %d\n", controller.blink);
+
+        puts("can send");
+		canSend(&can, 0x7DF, &controller, sizeof (Controller));
+	}
+}
+
 
 int main(void)
 {
+    std::thread controller_thread(ControllerThread);
+
+    cv::VideoCapture cap(0);
+
+    cap.set(cv::CAP_PROP_FRAME_WIDTH,  320 * 2);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 240 * 2);
+
+    Tilemap map;
+
+    cv::namedWindow("tilemap", cv::WINDOW_NORMAL);
+
+    cv::Mat capture;
+
+    int dialate_count = 0;
+
+	InterPosList klassification;
+
+    while (true) {
+        int key = cv::waitKey(100);
+
+        if (key == 27) break;
+        if (key == '1') dialate_count--;
+        if (key == '2') dialate_count++;
+
+        dialate_count = CLAMP(dialate_count, 0, 10);
+
+        cap >> capture;
+        
+        {
+            MatToEdge(capture);
+        }
+
+        TilemapResize(&map, capture.cols, capture.rows, 16);
+        TilemapClear(&map);
+
+      	// printf("%d %d\n", map.width, map.height);
+
+        {
+            TilemapFillEdges(&map, capture.ptr(), capture.cols, capture.rows);
+
+            for (int i = 0; i < dialate_count; ++i) {
+                TilemapDialate(&map);
+            }
+        }
+    
+        {
+            TilemapFloodFill(&map,&map, map.width / 2, map.height - 1, TILE_ROAD);
+        }
+
+        TilemapDrawRoadCenter(&map, &map,1);
+
+        RoadState state = GetRoadState(&map); // type
+        float     pos   = GetRoadPosition(&map,state); //pos
+
+		InterPos typePos;
+		typePos.type = state;
+		typePos.pos = pos;
+		
+		klassification.push(typePos);
+		klassification.analyze();
+
+		//printf("klass:  Blink %d\n" ,klassification.blink);
+		//printf("klass: PosAvg %.2f\n" ,klassification.pos);
+        //std::cout << klassification.pos << "\n";
+        //printf("klass: type %d\n", klassification.type);
+		//printf("klass:  PosDifAvg %.2f\n" ,klassification.posDifAvg);
+
+
+        //printf("position: %.2f\n", pos);
+        //printf("position: %d\n", typePos.type);
+        
+
+        //if (state & ROAD_UP)    puts("found up");
+        //if (state & ROAD_LEFT)  puts("found left");
+        //if (state & ROAD_RIGHT) puts("found right");
+    
+        {
+            cv::Mat tilemap = cv::Mat::zeros(map.height, map.width, CV_8UC3);
+
+            clock_t start = clock();
+
+            for (int y = 0; y < map.height; ++y) {
+                for (int x = 0; x < map.width; ++x) {
+                    int         tile    = TilemapGet(&map, x, y);
+                    cv::Vec3b&  pixel   = tilemap.at<cv::Vec3b>(y, x);
+
+                    switch (tile) {
+                        case TILE_EDGE:
+                            pixel = { 255, 0, 0 };
+                            break;
+                        case TILE_ROAD:
+                            pixel = { 0, 255, 0 };
+                            break;
+                        case TILE_CENTER:
+                            pixel = { 0, 100, 255 };
+                            break;
+                    }
+                }
+            }
+
+            clock_t end = clock();
+
+          //  printf("AsciiMap ms: %d\n", (int)(end - start));
+
+            cv::resizeWindow("tilemap", map.width * map.cell_size*0.5, map.height * map.cell_size*0.5);
+            cv::imshow("tilemap", tilemap);
+        }
+
+        //cv::resizeWindow("capture", capture.cols, capture.rows);
+       // cv::imshow("capture", capture);
+    }
+
+	return 0;
+}
+
+
+#if 0
 	Can 		can         = {0};
 	Server 		server      = {0};
 	Controller 	controller  = {0};
@@ -38,7 +181,7 @@ int main(void)
         puts("can send");
 		canSend(&can, 0x7DF, &controller, sizeof (Controller));
 	}
+#endif
 
-	return 0;
-}
+
 
